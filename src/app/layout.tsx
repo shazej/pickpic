fc8e3642bbedc489e37ed2b79459493365130031
Codeaxes Technologies <codeaxes18@gmail.com>
2025-12-06 18:39:26 +0000

    see the layouy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,7 +93,7 @@ export default function RootLayout({
                 </div>
               </nav>
             </header>
-            <main className="flex-grow">
+            <main className="flex-1">
               {children}
             </main>
             <Footer />
